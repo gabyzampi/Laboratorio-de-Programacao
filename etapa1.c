4331ab1127875c1a318938ebef26cc139250082c
gabyzampi <gabrielezampier36@gmail.com>
2025-08-07 22:28:14 -0300

    Add files via upload

---
+++ etapa1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void){
+    printf("\nHello Word\n");
+    printf("\nNome: Gabriele\n");
+    printf("\nIdade:18\n");
+    printf("\nCurso: Computacao\n");
+}
+
+/*poema*/
+
+int poema(void){
+    printf("Que a tristeza te convença\nQue a saudade não compensa\nE que a ausência não dá paz\nE o verdadeiro amor de quem se ama\nTece a mesma antiga trama\nQue não se desfaz\nE a coisa mais divina\nQue há no mundo\nÉ viver cada segundo\nComo nunca mais...");
+}
