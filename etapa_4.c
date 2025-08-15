85901f3a28934a37fc2c4db61d6a5535012e1bf0
Gabriele Zampier de Andrade <gabrielezampier36@gmail.com>
2025-08-14 22:27:02 -0300

    Create etapa_4.c

---
+++ etapa_4.c
@@ -0,0 +1,18 @@
+//etapa 4
+//10 numeros de 1 a 100
+
+#include <stdio.h>
+
+int main(void) {
+int contador = 1;
+ while (contador < 101) {
+     printf("Contador: %d\n", contador);
+     contador++;
+ }
+ return 0;}
+
+ //11
+ int numero,limite,contador;
+  printf("Insira qual sera o numero da tabuada:");
+  scanf("%d", %)
+
