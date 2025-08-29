dee1e68ec0e154fe55d5165fd444b670ae7aa976
Gabriele Zampier de Andrade <gabrielezampier36@gmail.com>
2025-08-29 12:12:55 -0300

    Add files via upload
    Etapas 1a 6 - C

--- etapa_4.c
+++ etapa_4.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 int main(void) {
-/*int contador = 1;
+int contador = 1;
  while (contador < 101) {
      printf("Contador: %d\n", contador);
      contador++;
@@ -21,7 +21,7 @@ int i, j;
         printf("%d x %d = %d\n", i, j, i * j);
     }
 } 
-}
+
 
 //12
 
@@ -38,80 +38,5 @@ int i, j;
     }
     
     printf("A soma dos numeros eh: %d\n", soma);
-    
-
-//13
-
-float numero1, numero2;
-
-printf("Insira um numero para operação:");
-scanf("%f", &numero1);
-printf("Insira outro numero para operação:");
-scanf("%f", &numero2);
-
-printf("Escolha um operador: (+),(-),(*),(/):");
-char operador;
-scanf(" %c", &operador);
-
-switch (operador) {
-    case '+':
-        printf("Resultado: %.2f\n", numero1 + numero2);
-        break;
-    case '-':
-        printf("Resultado: %.2f\n", numero1 - numero2);
-        break;
-    case '*':
-        printf("Resultado: %.2f\n", numero1 * numero2);
-        break;
-    case '/':
-        if (numero2 != 0) {
-            printf("Resultado: %.2f\n", numero1 / numero2);
-        } else {
-            printf("Erro: Divisão por zero não é permitida.\n");
-        }
-        break;
-    default:
-        printf("Operador inválido.\n");
-        break;
-}
-
-//14
-
-int temperaturaCel;
-int temperaturaFah; 
-int opcao;
-
-printf("Voce deseja coverter de C para F (1) ou de F para C (2)? ");
-scanf("%d", &opcao);
-
-if (opcao == 1) {
-    printf("Digite a temperatura em Celsius: ");
-    scanf("%d", &temperaturaCel);
-    printf("Temperatura em Fahrenheit: %.2f\n", (temperaturaCel * 9.0/5.0) + 32);
-} else if (opcao == 2) {
-    printf("Digite a temperatura em Fahrenheit: ");
-    scanf("%d", &temperaturaFah);
-    printf("Temperatura em Celsius: %.2f\n", (temperaturaFah - 32) * 5.0/9.0);
-} else {
-    printf("Opcao invalida.\n");
-}
-*/
-
-
-//15 Usando vetores (array), receba uma frase e retorne sua escrita inversa.
-
-char frase[100];
-printf ("Digite uma frase:");
-scanf("%c[^\n]", &frase);
-
-// Inverter a frase
-int i, j;
-char fraseInvertida[100];
-for (i = 0, j = strlen(frase) - 1; i < strlen(frase); i++, j--) {
-    fraseInvertida[i] = frase[j];
-}
-fraseInvertida[i] = '\0';
-
-printf("Frase invertida: %s\n", fraseInvertida);
-return 0;
+    return 0;
 }
