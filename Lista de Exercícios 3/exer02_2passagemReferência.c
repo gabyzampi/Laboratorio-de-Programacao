062fa46ea09062cfea431dae00e29faccb1ba999
Gabriele Zampier de Andrade <gabrielezampier36@gmail.com>
2025-10-20 11:47:56 -0300

    Add files via upload

---
+++ Lista de Exercícios 3/exer02_2passagemReferência.c
@@ -0,0 +1,11 @@
+/*2. Passagem por referência 
+Implemente uma função chamada trocar que receba dois inteiros por referência (via 
+ponteiro) e troque seus valores. 
+No main, peça ao usuário para digitar dois números, chame a função e mostre os valores 
+trocados. */
+
+int trocar(int*num1, int*num2){
+
+}
+
+
