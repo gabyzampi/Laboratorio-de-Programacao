ff7c5cc1b684b0ce6bb3eb903e812b74b16c7624
Gabriele Zampier de Andrade <gabrielezampier36@gmail.com>
2025-10-29 15:21:28 -0300

    Add files via upload

--- Lista de Exercícios 3/exer02_2passagemReferência.c
+++ Lista de Exercícios 3/exer02_2passagemReferência.c
@@ -4,8 +4,28 @@ ponteiro) e troque seus valores.
 No main, peça ao usuário para digitar dois números, chame a função e mostre os valores 
 trocados. */
 
-int trocar(int*num1, int*num2){
+#include <stdio.h>
 
+void trocar(int*num1, int*num2){
+    int aux;
+    aux = *num1;
+    *num1 = *num2;
+    *num2 = aux;
 }
 
+int main(){
 
+    int num1, num2;
+
+    printf("Digite um numero:");
+    scanf("%d", &num1);
+
+    printf("Digite outro numero:");
+    scanf("%d", &num2);
+
+    trocar(&num1,&num2);
+
+    printf("Os valores foram trocados: num1 = %d, num2 = %d\n", num1, num2);
+
+    return 0;
+}
