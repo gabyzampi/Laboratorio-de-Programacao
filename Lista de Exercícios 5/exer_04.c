0e961182a207c629f461596454a2c67b79ee3ddd
Gabriele Zampier de Andrade <gabrielezampier36@gmail.com>
2025-11-14 17:24:01 -0300

    Add files via upload

---
+++ Lista de Exercícios 5/exer_04.c
@@ -0,0 +1,8 @@
+/*Exercício 4 – Inserção em lista encadeada 
+Reutilize a struct Node do exercício anterior. 
+Agora crie funções: 
+● Node* inserirInicio(Node* inicio, int valor) que insere um elemento 
+no início da lista 
+● void imprimirLista(Node* inicio) que imprime a lista 
+Teste inserindo alguns elementos e exibindo a lista.*/
+
---
+++ Lista de Exercícios 5/exer_05.c
@@ -0,0 +1,7 @@
+/*Exercício 5 – Remoção em lista encadeada 
+Com a mesma struct Node, crie uma função: 
+● Node* removerInicio(Node* inicio) que remove o primeiro elemento da 
+lista e retorna o novo início. 
+Teste inserindo 3 elementos, imprimindo a lista, removendo o primeiro e imprimindo 
+novamente.*/
+
